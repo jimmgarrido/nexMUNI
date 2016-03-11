11c4b655a64975acf3cba0ab14e9d763df44e12e
Jimmy Garrido <jimmygarrido@outlook.com>
2016-03-10 21:54:07 -0800

    Use Dictionary instead of List

--- nexMuni/nexMuni.Shared/Helpers/DataRefreshHelper.cs
+++ nexMuni/nexMuni.Shared/Helpers/DataRefreshHelper.cs
@@ -2,11 +2,14 @@
 using SQLite;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.Web.Http;
 
 namespace nexMuni.Helpers
@@ -16,58 +19,39 @@ namespace nexMuni.Helpers
         public async Task RefreshDataAsync()
         {
             var routesList = new List<Route>();
-            var allStopsList = new List<Stop>();
+            //var allStopsList = new List<Stop>(1050);
+            var stopDictionary = new Dictionary<string, Stop>(1050);
             var baseUrl = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=";
 
 
             //Get XML listing all routes of SF Muni
             //var reader = XmlReader.Create("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni");
 
-            //var routesString = await GetData("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni");
-            //var routeDoc = XDocument.Parse(routesString);
+            var routesString = await GetDataString("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni");
+            var routeDoc = XDocument.Parse(routesString);
 
-            //var routeElements = routeDoc.Element("body").Elements("route");
+            var routeElements = routeDoc.Element("body").Elements("route");
 
-            ////Read each XML line and add the route number or letter to a list
-            //foreach (var routeElement in routeElements)
-            //{
-            //    var title = routeElement.Attribute("title").Value;
-            //    var num = routeElement.Attribute("tag").Value;
-            //    routesList.Add(new Route(title, num));
-            //}
+            //Read each XML line and add the route number or letter to a list
+            foreach (var routeElement in routeElements)
+            {
+                var title = routeElement.Attribute("title").Value;
+                var num = routeElement.Attribute("tag").Value;
+                routesList.Add(new Route(title, num));
+            }
 
-            //Go through each route in RouteNum and append to the base URL. Get XML from each URL and parse
-            //foreach(var route in routesList)
-            //{
-            //    var stopsUrl = string.Join(null, baseUrl, route.RouteNumber);
-            //    var stopsString = await GetData(stopsUrl);
-
-            //    var stopsDoc = XDocument.Parse(stopsString);
-            //    var stopElements = stopsDoc.Element("body").Element("route").Elements("stop");
-
-            //    foreach(var element in stopElements)
-            //    {
-            //var name = element.Attribute("title").Value;
-            //var stopTag = string.Join("|", route.RouteNumber, element.Attribute("tag").Value);
-            //var lon = double.Parse(element.Attribute("lon").Value);
-            //var lat = double.Parse(element.Attribute("lat").Value);
-            //var num = route.RouteNumber;
-
-            //allStopsList.Add(new Stop(name, stopTag, num, lat, lon));
-            //    }
-            //}
-
-            var routeData = await GetData("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni");
+            var routeData = await GetDataString("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni");
             var routeDataDoc = XDocument.Parse(routeData);
-            var routeDataElements = routeDataDoc.Element("body").Elements("route");
+            //var routeDataElements = routeDataDoc.Element("body").Elements("route"); 
+            var routeDataElements = routeDataDoc.Element("body").Elements();
 
-            foreach(var route in routeDataElements)
+            foreach (var route in routeDataElements)
             {
                 var title = route.Attribute("title").Value;
                 var num = route.Attribute("tag").Value;
                 routesList.Add(new Route(title, num));
 
-                foreach(var stop in route.Elements("stop"))
+                foreach (var stop in route.Elements("stop"))
                 {
                     //var name = stop.Attribute("title").Value;
                     var stopTag = string.Join("|", num, stop.Attribute("tag").Value);
@@ -94,9 +78,24 @@ namespace nexMuni.Helpers
                         //stop.StopName = key;
                     }
 
-                    if (allStopsList.Exists(x => x.StopName == key))
+                    //if (allStopsList.Exists(x => x.StopName == key))
+                    //{
+                    //    var matchedStop = allStopsList.Find(x => x.StopName == key);
+                    //    matchedStop.StopTags = string.Join(",", matchedStop.StopTags, stopTag);
+
+                    //    if (!matchedStop.Routes.Contains(routeNum))
+                    //    {
+                    //        matchedStop.Routes = string.Join(", ", matchedStop.Routes, routeNum);
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    allStopsList.Add(new Stop(key, stopTag, num, lat, lon));
+                    //}
+
+                    if (stopDictionary.ContainsKey(key))
                     {
-                        var matchedStop = allStopsList.Find(x => x.StopName == key);
+                        var matchedStop = stopDictionary[key];
                         matchedStop.StopTags = string.Join(",", matchedStop.StopTags, stopTag);
 
                         if (!matchedStop.Routes.Contains(routeNum))
@@ -106,82 +105,37 @@ namespace nexMuni.Helpers
                     }
                     else
                     {
-                        allStopsList.Add(new Stop(key, stopTag, num, lat, lon));
+                        stopDictionary.Add(key,new Stop(key, stopTag, num, lat, lon));
                     }
                 }
             }
 
-            //Prepare to sort all the stops for duplicates
-            //var key = string.Empty;
-            //var sortedList = new List<Stop>();
+            string[] splitName;
+            string reversedName = string.Empty;
 
-            //foreach(var stop in allStopsList)
+            ////List method
+            //for (int i = 0; i < allStopsList.Count; i++)
             //{
-            //    key = stop.StopName;
-
-            //    //Remove "Inbound/Outbound" from metro station names
-            //    if (key.Contains("Outbound"))
-            //    {
-            //        key = key.Remove(key.IndexOf("Outbound") - 1);
-            //        stop.StopName = key;
-            //    }
-            //    else if (key.Contains("Inbound"))
-            //    {
-            //        key = key.Remove(key.IndexOf("Inbound") - 1);
-            //        stop.StopName = key;
-            //    }
-            //    else if (key.Contains("OB") || key.Contains("IB"))
-            //    {
-            //        key = key.Remove(key.Length - 2);
-            //        stop.StopName = key;
-            //    }
-
-            //    if (sortedList.Exists(x => x.StopName == key))
-            //    {
-            //        var matchedStop = sortedList.Find(x => x.StopName == key);
-            //        matchedStop.StopTags = string.Join(",", matchedStop.StopTags, stop.StopTags);
-
-            //        if(!matchedStop.Routes.Contains(stop.Routes))
-            //        {
-            //            matchedStop.Routes = string.Join(", ", matchedStop.Routes, stop.Routes);
-            //        }
-            //    }
-            //    else
-            //    {
-            //        sortedList.Add(stop);
-            //    }
-            //}
-
-
-            //Prepare for second sort to remove similar bus stops e.g. 5th & Mission St / Mission St. & 5th St
-            //string[] splitName;
-            //string reversedName = string.Empty;
-
-            //for(int i=0; i<sortedList.Count; i++)
-            //{
-            //    var stop = sortedList[i];
+            //    var stop = allStopsList[i];
             //    splitName = stop.StopName.Split(new string[] { " & " }, StringSplitOptions.None);
 
-            //    if(splitName.Length > 1)
+            //    if (splitName.Length > 1)
             //    {
             //        reversedName = string.Join(" ", splitName[1], "&", splitName[0]);
             //    }
 
-            //    if(sortedList.Exists(x => x.StopName == reversedName))
+            //    if (allStopsList.Exists(x => x.StopName == reversedName))
             //    {
-            //        var matchedStop = sortedList.Find(x => x.StopName == reversedName);
+            //        var matchedStop = allStopsList.Find(x => x.StopName == reversedName);
             //        stop.StopTags = string.Join(",", stop.StopTags, matchedStop.StopTags);
 
-            //        sortedList.Remove(matchedStop);
+            //        allStopsList.Remove(matchedStop);
             //    }
             //}
 
-            string[] splitName;
-            string reversedName = string.Empty;
-
-            for (int i = 0; i < allStopsList.Count; i++)
+            for (int i = 0; i < stopDictionary.Count; i++)
             {
-                var stop = allStopsList[i];
+                var stop = stopDictionary.Values.ElementAt(i);
                 splitName = stop.StopName.Split(new string[] { " & " }, StringSplitOptions.None);
 
                 if (splitName.Length > 1)
@@ -189,16 +143,17 @@ namespace nexMuni.Helpers
                     reversedName = string.Join(" ", splitName[1], "&", splitName[0]);
                 }
 
-                if (allStopsList.Exists(x => x.StopName == reversedName))
+                if (stopDictionary.ContainsKey(reversedName))
                 {
-                    var matchedStop = allStopsList.Find(x => x.StopName == reversedName);
+                    var matchedStop = stopDictionary[reversedName];
                     stop.StopTags = string.Join(",", stop.StopTags, matchedStop.StopTags);
 
-                    allStopsList.Remove(matchedStop);
+                    stopDictionary.Remove(reversedName);
                 }
             }
 
 
+            #region WriteDatabase
             await ApplicationData.Current.LocalFolder.CreateFileAsync("refresh.sqlite",CreationCollisionOption.ReplaceExisting);
             var refreshDb = await ApplicationData.Current.LocalFolder.GetFileAsync("refresh.sqlite");
 
@@ -207,7 +162,11 @@ namespace nexMuni.Helpers
             //var _favoritesAsyncConnection = new SQLiteAsyncConnection(() => new SQLiteConnectionWithLock(new SQLitePlatformWinRT(), new SQLiteConnectionString(favoriteDbPath, false)));
 
             await _refreshAsyncConnection.CreateTableAsync<Stop>();
-            foreach(var stop in allStopsList)
+            //foreach(var stop in allStopsList)
+            //{
+            //    await _refreshAsyncConnection.InsertAsync(stop);
+            //}
+            foreach (var stop in stopDictionary.Values)
             {
                 await _refreshAsyncConnection.InsertAsync(stop);
             }
@@ -234,9 +193,10 @@ namespace nexMuni.Helpers
             {
                 
             }
+#endregion
         }
 
-        private async Task<string> GetData(string url)
+        private async Task<string> GetDataString(string url)
         {
             string response = string.Empty;
 
@@ -257,5 +217,6 @@ namespace nexMuni.Helpers
 
             return response;
         }
+
     }
 }
