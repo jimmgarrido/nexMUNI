579bcab79d2547ff7766959056f7f07f891f2fb9
Jimmy Garrido <jimmygarrido@outlook.com>
2016-08-06 22:39:10 -0700

    [UWP] Request access to location

--- nexMuni/nexMuni.Shared/Helpers/LocationHelper.cs
+++ nexMuni/nexMuni.Shared/Helpers/LocationHelper.cs
@@ -27,6 +27,21 @@ namespace nexMuni.Helpers
         {
             await UIHelper.ShowStatusBar("Getting Location");
 
+#if !WINDOWS_PHONE_APP
+            var accessStatus = await Geolocator.RequestAccessAsync();
+
+            switch(accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+                    if (geolocator == null) geolocator = new Geolocator { DesiredAccuracyInMeters = 50 };
+                    Location = await geolocator.GetGeopositionAsync(TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(30));
+                    latitude = Point.Position.Latitude;
+                    longitude = Point.Position.Longitude;
+                    LocationChanged?.Invoke();
+                    break;                    
+            }
+#else
+
             if (geolocator == null) geolocator = new Geolocator { DesiredAccuracyInMeters = 50 };
             if (geolocator.LocationStatus == PositionStatus.Disabled)
             {
@@ -39,6 +54,7 @@ namespace nexMuni.Helpers
                 longitude = Point.Position.Longitude;
                 LocationChanged?.Invoke();
             }
+#endif
 
             await UIHelper.HideStatusBar();
         }
